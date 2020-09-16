187133bb85cf728094d3a81265cd5b17fe9f9932
Simret <simruineb@gmail.com>
2020-09-17 01:03:46 +0300

    Adding js file-openweather api and local storage

--- main.js
+++ main.js
@@ -12,23 +12,42 @@ console.log(position);
 })
 */
 
-var button = document.querySelector('.button');
-var inputValue = document.querySelector('.inputValue');
-var name = document.querySelector('.name');
-var desc = document.querySelector('.desc');
-var temp = document.querySelector('.temp');
+const button = document.querySelector('.button');
+const inputValue = document.querySelector('.inputValue');
+const name = document.querySelector('.name');
+const desc = document.querySelector('.desc');
+const temp = document.querySelector('.temp');
 
-button.addEventListener('click', function(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=d57221989602f0af81a99a0fdf08a1be')
+let weatherArray = [];
+
+
+const GetWeather = (Location) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${Location}&appid=d57221989602f0af81a99a0fdf08a1be`;
+    fetch(url)
 .then(response => response.json())
 .then(data => {
-    var nameValue = data['name'];
-    var tempValue = data['main']['temp'];
-    var descValue = data['weather'][0]['description'];
+weatherArray.push(data);
+    const nameValue = data['name'];
+    const tempValue = data['main']['temp'];
+    const descValue = data['weather'][0]['description'];
 
     name.innerHTML = nameValue;
     temp.innerHTML = tempValue;
     desc.innerHTML = descValue;
+    localStorage.setItem('weatherdata', JSON.stringify(weatherArray))
 })
-.catch(err => alert("Wrong city name!"))
+.catch((error) => {
+    console.log(error);
 });
+};
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+
+    GetWeather(inputValue.value);
+    
+}
+
+
+button.addEventListener('click', handleSubmit);
+console.log(weatherArray);
